2a09cb4a614dbf5d2c55209cc7224ede9bb7d862
Kiarash Iranpour <kia.iranpour1380@gmail.com>
2022-04-28 23:36:13 +0430

    styles modified

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <div className="aboutUs__container">
+      <h2>About us</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto facilis
+        repellendus consequuntur quae cumque ipsa minus laudantium repudiandae
+        blanditiis impedit, maiores pariatur modi commodi possimus eum soluta
+        deserunt aliquam. Fuga!
+      </p>
+    </div>
+  );
 }
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -5,7 +5,6 @@ import Contact from "./Contact";
 
 export default function Contacts() {
   const state = useSelector((state) => state);
-  console.log(state);
   return (
     <div className="contacts__container">
       <AddNewContact />
